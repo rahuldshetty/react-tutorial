845e3e2369ce7557cec3774dc5e79d9979b65ba0
Rahul <35rahuldshetty@gmail.com>
2020-11-22 23:32:44 +0530

    created todoapp

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,21 +1,52 @@
 import React, { Component } from 'react';
-import Ninjas from './Ninjas'
+import Ninjas from './Ninjas';
+import AddNinja from './AddNinja';
 
 class App extends Component{
   state = {
     ninjas: [
       {name:'Rahul', age:30, belt: 'black', id: 1},
-      {name:'Yoshi', age:25, belt: 'green', id: 2},
+      {name:'Yoshi', age:10, belt: 'green', id: 2},
       {name:'John', age:18, belt: 'red', id: 3},
       {name:'Jane', age:26, belt: 'pink', id: 4}
     ]
   }
+
+  addNinja = (ninja) => {
+    // Spreader
+    ninja.id = this.state.ninjas.length + 1;
+    let ninjas = [...this.state.ninjas, ninja];
+    this.setState({
+      ninjas : ninjas
+    })
+  }
+
+  deleteNinja = (id) => {
+    console.log(id)
+    let ninjas = this.state.ninjas.filter( ninja => {
+      return ninja.id != id
+    });
+    this.setState({
+      ninjas: ninjas
+    })
+  }
+
+  componentDidMount(){
+    console.log("Component MOunted...")
+  }
+
+  componentDidUpdate(prevProp, prevState){
+    console.log("Updated....");
+    console.log(prevProp, prevState);
+  }
+
   render(){
     return (
       <div className="App">
         <h1>My first React App!</h1>
         <p>Welcome :)</p>
-        <Ninjas ninjas={this.state.ninjas}/>
+        <Ninjas ninjas={this.state.ninjas} deleteNinja={this.deleteNinja}/>
+        <AddNinja addNinja={this.addNinja} />
       </div>
     );
   }
--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,25 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
+import './Ninjas.css';
 
-class Ninjas extends Component{
-    render(){
-        // Destructuring
-        // const { name, age, belt} = this.props;
-        const {ninjas} = this.props;
-        const ninjaList = ninjas.map(ninja => {
-            return (
-                <div className="ninja" key={ninja.id}>
-                    <div>Name: {ninja.name}</div>
-                    <div>Age: {ninja.age}</div>
-                    <div>Belt: {ninja.belt}</div>
-                </div>
-            )
-        })
-        return(
-            <div className="ninja-list">
-                { ninjaList }
-            </div>
-        )
-    }
+const Ninjas = ({ninjas, deleteNinja}) => {
+    // Destructuring
+    // const { name, age, belt} = this.props;
+    // const ninjaList = ninjas.map(ninja => { 
+    //     if (ninja.age > 20){
+    //         return (
+    //             <div className="ninja" key={ninja.id}>
+    //                 <div>Name: {ninja.name}</div>
+    //                 <div>Age: {ninja.age}</div>
+    //                 <div>Belt: {ninja.belt}</div>
+    //             </div>
+    //         )
+    //     }   else {
+    //         return null
+    //     }
+    // })
+
+    return(
+        <div className="ninja-list">
+        {
+            ninjas.map(ninja => {
+                return ninja.age > 20 ? (
+                    <div className="ninja" key={ninja.id}>
+                        <div>Name: {ninja.name}</div>
+                        <div>Age: {ninja.age}</div>
+                        <div>Belt: {ninja.belt}</div>
+                        <button onClick={ () => {deleteNinja(ninja.id)}}>Delete</button>
+                    </div>
+                ) : null;
+            })
+        }
+        </div>
+    )
 }
 
 export default Ninjas
